1dbfc40b64415d8816582b51f5ef9eefa02aca23
Matthew Schug <mschug@nicsroyals.com>
2017-04-05 14:18:57 -0700

    Here it is.

---
+++ d.py
@@ -0,0 +1,49 @@
+# Exercise 1
+
+a = {"puella":"girl","pulchra":"pretty","puer":"boy","fortis":"strong"}
+
+# write the expression that will retrieve the word 'pretty'.
+
+# write the expression that will retrieve the word 'boy'.
+
+# write the expression that will add a new key-value pair
+# consisting of the key 'laborare' and the value 'to work'
+
+# Exercise 2
+
+b = {"billy":{},"bobby":{"HW1":96},"barry":{"HW1":97.7}}
+
+# Who didn't turn an assignment in?
+
+# How would I update that person's grade for that assignment
+# to a 85.4?
+
+# Add another assignment 'HW2' with the grades 79,89,85
+# respectively for 'billy','bobby', and 'barry'.
+
+# Add another student 'bonny', and give her grade 100, and 99
+# for assignments 'HW1', and 'HW2' respectively.
+
+# Exercise 3
+c = [{"asdf":[1,2,3,4],"fdsa":[4,3,2,1]},
+     {"sdaf":[4,1,2,"uiopu"],
+      "fasd":[{"fds":["jklj","nmbn",4,67.7,0],"ffd":9},
+              [5,{"fffsa":"ddfs","ffdssaa":"fffdddsssa"}]]},
+     {"ffaaaaaasddfs":[0,1,3,4,5],"fdsfafsfds":[55,6,4,2]}]
+
+# Write the code to retrieve each of the following items
+# from dictionary c
+
+# [1,2,3,4]
+
+# [4,3,2,1]
+
+# 67.7
+
+# "fffdddsssa"
+
+# 55
+
+# "ddfs"
+
+# "nmbn"
